e8301e55463d8a22e5092df5640b64de6a77483c
drigoosouza <drigo250601souza@gmail.com>
2025-10-31 23:01:12 -0300

    roos com envio de email

--- script/Emailjs-init.js
+++ script/Emailjs-init.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', async function sendemail(e) {
   button.style.opacity = "0.7"
   
   try {
-      const send = await fetch("https://roos-main-n1pjoqncj-drigoosouzas-projects.vercel.app/enviar", {
+      const send = await fetch("https://roos-main-n1pjoqncj-drigoosouzas-projects.vercel.app", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, lastname, email, content, message })
